25120a4353db87b8f698b15ae9fdb69e7ca96473
pisarews <pisaqtom@gmail.com>
2020-08-06 12:50:09 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections;
+using System.Runtime.CompilerServices;
+
+namespace Algorithms_and_Data_Structures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Sorting Algorithms !");
+            int[] examplearray = {
+            0,21,40,10,31,5,6,9
+            };
+            Console.WriteLine("array before Sorting\n");
+            foreach(var item in examplearray)
+            {
+                Console.Write(item + " "); 
+            }
+
+            Console.WriteLine();
+            DateTime Start = DateTime.Now;
+            QuickSort<int>.Sort(examplearray);
+            Console.Write("\nTime of Sorting exec.: "); Console.WriteLine(DateTime.Now - Start); 
+            Console.WriteLine("array after Sorting\n");
+            foreach (var item in examplearray)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine(); Console.WriteLine();
+            
+
+            ArrayList tablicaList = new ArrayList(); //System.Collections 
+            tablicaList.Add(5);
+            tablicaList.AddRange(new int[] { 5, -7, 645 }); // obiekty są typo Object 
+
+        } 
+    }
+
+    class SortingAlgorithms 
+    {
+  
+
+        public static int[] SelectionSort(int[] array) // Big O Notation: O(n^2) because we have two for statements
+        {
+            int minIndex, minValue;
+            minIndex = 0; 
+            for(int i=0; i < array.Length; i++)
+            {
+                bool isSwapped = false;
+                minValue = array[i]; 
+                for(int j = i; j < array.Length; j++)
+                {
+                     if(array[j] < minValue)
+                     {
+                        minIndex = j;
+                        minValue = array[j]; 
+                        isSwapped = true; 
+                     }
+                }
+                if(isSwapped)
+                Swap(ref array[i], ref array[minIndex]); 
+            }
+            return array; 
+        }
+
+        public static int[] InsertionSort(int[] array) // Big O Notation: O(n^2) because we have a for and while statements
+        {
+            for(int i = 0; i < array.Length; i++)
+            {
+                int j = i;
+                while (j > 0 && array[j].CompareTo(array[j-1]) < 0)
+                {
+                    Swap(ref array[j], ref array[j - 1]);
+                    j--;
+                }
+            }
+            return array; 
+        }
+
+        public static int[] BubbleSort(int[] array) // Big O Notation: O(n^2) because we have a for and while statements
+        {
+            
+            for(int i = 0; i < array.Length; i++)
+            {
+                bool isSwapped = false;
+                for (int j = 0; j < array.Length - 1; j++)
+                {
+                    if(array[j].CompareTo(array[j + 1]) > 0)
+                    {
+                        isSwapped = true;
+                        Swap(ref array[j+1], ref array[j]); 
+                    }
+                    
+                }
+                if (!isSwapped)
+                {
+                    break;
+                }
+            }
+            return array; 
+        }
+
+
+        public static void Swap(ref int x, ref int y) 
+        {
+            int z = x;
+            x = y;
+            y = z;
+            return; 
+        }
+
+
+    }
+}
---
+++ QuickSort.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms_and_Data_Structures
+{
+    public class QuickSort<T> where T : IComparable // O(n log n) | O(n^2)
+    {
+        public static void Sort(T[] array)
+        {
+            Sort(array, 0, array.Length - 1); 
+        }
+
+        private static T[] Sort(T[] array, int lower, int upper)
+        {
+            if (lower < upper)
+            {
+                int p = Partition(array, lower, upper);
+                Sort(array, lower, p);
+                Sort(array, p + 1, upper); 
+            }
+            return array; 
+        }
+
+        private static int Partition(T[] array, int lower, int upper)
+        {
+            int i = lower;
+            int j = upper;
+            T pivot = array[lower];
+
+            do
+            {
+                while (array[i].CompareTo(pivot) < 0) { i++; }
+                while (array[j].CompareTo(pivot) > 0) { j-- ; }
+                if(i >= j) { break;  }
+                Swap(ref array[i], ref array[j]); 
+            } while (i <= j); 
+
+            return j; 
+        }
+
+        public static void Swap(ref T x, ref T y)
+        {
+            T z = x;
+            x = y;
+            y = z;
+            return;
+        }
+    }
+}
